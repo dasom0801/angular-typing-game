542c0224799fbd310ed0e2aa16e2db8e0466eee7
dasom <dev.dasom1012@gmail.com>
2019-06-14 14:28:47 +0900

    추가: 라우팅 설정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent }      from './main/main.component';
+import { PlayComponent }      from './play/play.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'main', component: MainComponent },
+  { path: 'play', component: PlayComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PlayComponent,
   ],
   imports: [
     BrowserModule,
